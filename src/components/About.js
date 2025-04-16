e3cf1979570f0d5e0eaf0863d381715bde74515a
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-04-16 08:56:33 +0300

    adds files

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
-
+console.log(image);
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about me.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
